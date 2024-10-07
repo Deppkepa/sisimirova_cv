4da2035a1ba50e690a0365ee85208abec90a1331
Даша <dasha.sisimirova@bk.ru>
2024-10-07 14:40:40 +0800

    Intermediate result

--- gradient/main.py
+++ gradient/main.py
@@ -1,8 +1,10 @@
-​import numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
 
 def lerp(v0, v1, t):
     return (1 - t) * v0 + t * v1
+def t(x,y,size):
+    return (x + y) / (2 * (size - 1))
 
 size = 100
 image = np.zeros((size, size, 3), dtype="uint8")
@@ -11,12 +13,19 @@ assert image.shape[0] == image.shape[1]
 color1 = [255, 128, 0]
 color2 = [0, 128, 255]
 
-for i, v in enumerate(np.linspace(0, 1, image.shape[0])):
-    r = lerp(color1[0], color2[0], v)
-    g = lerp(color1[1], color2[1], v)
-    b = lerp(color1[2], color2[2], v)
-    image[i, :, :] = [r, g, b]
+# for i, v in enumerate(np.linspace(0, 1, image.shape[0])):
+#     r = lerp(color1[0], color2[0], v)
+#     g = lerp(color1[1], color2[1], v)
+#     b = lerp(color1[2], color2[2], v)
+#     image[i, :, :] = [r, g, b]
+
+for x in range(image.shape[0]):
+    for y in range(image.shape[1]):
+        r = lerp(color1[0], color2[0], t(x, y, image.shape[0]))
+        g = lerp(color1[1], color2[1], t(x, y, image.shape[0]))
+        b = lerp(color1[2], color2[2], t(x, y, image.shape[0]))
+        image[x, y] = [b, g, r]
 
 plt.figure(1)
-plt.imshow(image)
+plt.imshow(image, vmin=50, vmax=100)
 plt.show()
